f15ee1ca75df853f474e9759bdf139ca1ac0bc2d
s303126 <s303126@students.cdu.edu.au>
2025-09-08 16:08:51 +0930

    Updated descriptive statistics for hypothesis5.py

--- hypothesis5.py
+++ hypothesis5.py
@@ -1,9 +1,50 @@
 import pandas as pd
 from scipy.stats import ttest_ind
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('dataset2.csv')
+df = pd.read_csv('cleaned_dataset2.csv')
 
-#TODO:clean data
+median = df['rat_minutes'].median()
+print(median)
+df['rat_present'] = df['rat_minutes'] > median
+print(df['rat_present'].value_counts())
+
+rat_present = df.loc[df['rat_present'], 'bat_landing_number']
+rat_absent = df.loc[~df['rat_present'], 'bat_landing_number']
+print("presence: ", rat_present.mean())
+print("absence: ", rat_absent.mean()) # more traffic when less rats
+
+mean_present = rat_present.mean()
+median_present = rat_present.median()
+present_standard_deviation = rat_present.std(ddof=1)
+
+mean_absent = rat_present.mean()
+median_absent = rat_absent.median()
+absent_standard_deviation = rat_absent.std(ddof=1)
+
+
+#IQR for present and absent
+
+
+#TODO histograms
+max_val_present = rat_present.max()
+min_val_present = rat_present.min()
+the_range_present = max_val_present - min_val_present
+bin_width = 5
+bin_count_present= int(the_range_present/bin_width)
+
+max_val = rat_absent.max()
+min_val = rat_absent.min()
+the_range = max_val - min_val
+bin_count= int(the_range/bin_width)
+
+plt.hist(rat_present, bins=bin_count_present, alpha=0.6, label='Risky Reward Bats', color='red', edgecolor='black')
+plt.hist(rat_absent, bins=bin_count, alpha=0.6, label='Avoidant Reward Bats', color='green', edgecolor='black')
+plt.title('Bat Presence for High and Low Rat Presence')
+plt.xlabel('Rat Presence')
+plt.ylabel('Bat Frequency')
+plt.show()
+
 '''
 Hypothesis 5: Rat Presence Reduces Bat Traffic<br />
 Variables:<br />
